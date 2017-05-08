11bea1afc46720473d9f625a0e29835ec7ca2eeb
B505 <836215109@qq.com>
2017-05-09 02:17:30 +0800

    config database

--- src/cn/banking/controller/AdminController.java
+++ src/cn/banking/controller/AdminController.java
@@ -1,8 +1,12 @@
-package cn.myapp.controller;
+package cn.banking.controller;
+
+import java.util.List;
 
 import com.jfinal.core.Controller;
 
-public class IndexController extends Controller {
+import cn.banking.model.AdminInfo;
+
+public class AdminController extends Controller {
 	public void index() {
 
 		this.render("/index.jsp");
@@ -14,7 +18,11 @@ public class IndexController extends Controller {
 		String userName = this.getPara("userName");
 
 		String sayHello = "Hello " + userName + "，welcome to JFinal world.";
-
+		
+		List users = AdminInfo.dao.find("SELECT * FROM admin_info WHERE admin_account = '" + userName + "'");
+		
+        setAttr("size", users.size());
+        
 		this.setAttr("sayHello", sayHello);
 
 		this.render("/hello.jsp");
---
+++ src/cn/banking/model/AdminInfo.java
@@ -0,0 +1,7 @@
+package cn.banking.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class AdminInfo extends Model<AdminInfo>{
+    public static final AdminInfo dao = new AdminInfo();
+}
